5fc12703cb67c28d38f9202067cbc0db331d096b
Nirvaan Aggarwal <ashish@thesilkfactory.net>
2020-04-19 10:41:09 +0530

    addplatform

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,platform;
 var backgroundImg,platform;
 
 function preload() {
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform = new Ground(180,305,300,170);
 
 }
 
